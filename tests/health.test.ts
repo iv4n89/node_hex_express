5a02ad583c86585c52f23e9a16e6a8c1bd0d7938
iv4n89 <ivan.betanzos89@gmail.com>
2025-04-10 01:27:09 +0200

    chore: update .gitignore to include .tmp and ensure .env is ignored; add health check test file

---
+++ tests/health.test.ts
@@ -0,0 +1,7 @@
+
+// This test is a placeholder to ensure that the test suite runs correctly and can be used to check the health of the testing environment.
+describe('Health Check for tests', () => {
+    it('should be true', () => {
+        expect(true).toBe(true);
+    })
+})
